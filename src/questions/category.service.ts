a77182eded87cdd9e49bab538e19905af15dbbb3
nadineboukadida <nadineboukadida@insat.u-carthage.tn>
2022-05-28 10:07:10 +0200

    fix/dependencies for post service

---
+++ src/questions/category.service.ts
@@ -0,0 +1,19 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { CrudService } from "src/misc/crud";
+import { Repository } from "typeorm";
+import { Category } from "./entities/category.entity";
+
+export class categoryService extends CrudService<Category> {
+
+    constructor(
+      @InjectRepository(Category)
+      private categoryService: Repository<Category>,
+    ) {
+      super(categoryService);
+    }
+
+    async getCategoryById(id: string,): Promise<Category> {
+      return await this.categoryService.findOne({where:{ id }});
+    }
+  
+  }
